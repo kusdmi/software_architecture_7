51490a4fd3b34959381983714a96f546d3ea1499
kusdmi <kusdmi@yandex.ru>
2026-03-28 16:01:23 +0300

    update files_2

--- auth_service/app/schemas.py
+++ auth_service/app/schemas.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, EmailStr, Field
 
 class UserCreate(BaseModel):
     email: EmailStr
-    password: str = Field(..., min_length=8)
+    password: str = Field(..., min_length=8, max_length=72)
 
 class UserLogin(BaseModel):
     email: EmailStr
